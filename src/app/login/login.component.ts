6b3d5197805788ecffa5aa954836815df6a034cf
quangloc.nguyen <you@example.com>
2020-11-17 13:36:31 +0700

    google login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import firebase from 'firebase/app'
+import { AngularFireAuth } from '@angular/fire/auth'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private c: AngularFireAuth) {
+  }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.c.signInWithPopup(new firebase.auth.GoogleAuthProvider())
   }
 
 }

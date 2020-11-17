f6dfe5cb65e4ef578e91e2dd020bcbca2252322a
quangloc.nguyen <you@example.com>
2020-11-17 14:42:04 +0700

    async pipe

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth'
 import  firebase from 'firebase'
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'bs-navbar',
+  selector: 'bs-navbar', 
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent  {
-  user : firebase.User;
+  // user : firebase.User;
+  user$: Observable<firebase.User>
 
   constructor(private c: AngularFireAuth) { 
     c.authState.subscribe(e => {
-      this.user = e
+      this.user$ = c.authState;
       console.log(e);
       
     })
